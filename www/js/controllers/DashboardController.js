559c3d03aca28aa102e147d5be591009badec94d
Dheeraj <dheeraj.pal@rubiconred.com>
2017-09-18 21:10:12 +0530

    user based activity

--- www/js/controllers/DashboardController.js
+++ www/js/controllers/DashboardController.js
@@ -17,8 +17,16 @@ angular.module('starter.controllers')
     $http(req).then(function (data) {
       if (data.data != "") {
         //console.log(data.data);
+        $scope.engageData = data.data;
         $scope.urlParam = data.data.name;
         $scope.imageUrl = data.data.imageurl;
+        var manufacture = new Date(data.data.manufacture_date);
+        var expire = new Date(data.data.expire_date);
+        if (manufacture < expire) {
+          $scope.freshInfo = true;
+        } else {
+          $scope.freshInfo = false;
+        }
       } else {
         $scope.urlParam = "Frozen Ghost1";
         $scope.imageUrl = "http://1.bp.blogspot.com/-x0qgbWJI6ZY/TlL86fmHErI/AAAAAAAAygc/hUIEBa8DXnM/s1600/lovely-package-frozen-ghost-vodka1-e1313218158662.jpg";
